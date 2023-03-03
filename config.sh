687cb5478cf2680af67965563b86f277ab14a8a8
Abhinav Verma <79688470+RemuruSama@users.noreply.github.com>
2023-03-03 09:02:26 -0500

    update for ax5

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-KERNEL_SOURCE=https://github.com/RemuruSama/Realme_2_C1-AndroidP-kernel-source.git
+KERNEL_SOURCE=https://github.com/oppo-source/AX5-8.1-kernel-source.git
 BRANCH=master
 KERNEL_VERSION=msm-4.9
 PLATFORM=*-89xx.0
