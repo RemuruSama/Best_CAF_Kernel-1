b12bcf0f826999c30d3770ad2f06b4fea9e0b3ee
Abhinav Verma <79688470+RemuruSama@users.noreply.github.com>
2023-04-22 11:24:15 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-KERNEL_SOURCE=https://github.com/oppo-source/AX5-8.1-kernel-source.git
-BRANCH=master
+KERNEL_SOURCE=https://github.com/RemuruSama/android_kernel_lenovo_TB-X505X.git
+BRANCH=android-9.0
 KERNEL_VERSION=msm-4.9
 PLATFORM=*-89xx.0
