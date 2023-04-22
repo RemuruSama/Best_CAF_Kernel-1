54ad1cd28f3f49653f51f45709119aa390b8902d
Abhinav Verma <79688470+RemuruSama@users.noreply.github.com>
2023-04-22 11:52:04 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-KERNEL_SOURCE=https://github.com/RemuruSama/android_kernel_lenovo_TB-X505X.git
+KERNEL_SOURCE=https://github.com/RemuruSama/android_kernel_lenovo_TB-X505X
 BRANCH=android-9.0
 KERNEL_VERSION=msm-4.9
 PLATFORM=*-89xx.0
